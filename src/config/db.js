ab3a9e4a87c3a49327395eb75bc3116bee89bb5a
Carlos Bedoya <164281122+chakymv@users.noreply.github.com>
2025-05-27 18:29:04 -0500

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,12 @@
 const mysql = require('mysql2/promise');
 
-// Es buena práctica cargar las variables de entorno al inicio de tu aplicación principal (ej. server.js)
-// usando require('dotenv').config(); si utilizas un archivo .env
 
-// Es recomendable usar variables de entorno para esta configuración
-// Por ejemplo, process.env.DB_HOST, process.env.DB_USER, etc.
-// Ahora usaremos variables de entorno.
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Ch2917892', // Considera eliminar el fallback de la contraseña en producción
+  password: process.env.DB_PASSWORD || 'contrasena',
   database: process.env.DB_NAME || 'parqueadeR_ssd',
-  port: process.env.DB_PORT || 3306, // Añadido para el puerto
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
